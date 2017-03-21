ffbb9bb8af6eee062d53cfdfbb020cd5553ab248
DESKTOP-0L8INH9\shijing <shijingoooo@gmail.com>
2017-03-21 16:21:46 +0800

    简单的表单

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  myHero = this.heroes[0];
+  clickMessage = '';
+  loginControl: FormControl = new FormControl('');
+  onClick(){
+    this.clickMessage = 'you are my hero';
+  }
+}
+class Car{
+  public engine: string;
+  constructor(engine: string){
+    this.engine = engine;
+  }
+  drive(distanceInMeters: string = '0'): string{
+    return distanceInMeters;
+  }
+}
+class MotoCar extends Car {
+  constructor(engine: string) {
+    super(engine);
+  }
+  drive(distanceInMeters: string = '50'): string{
+    return super.drive(distanceInMeters);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroFormComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
